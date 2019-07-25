1f297c25b31bcfdd365cb209c34fc35a8930acbb
Twulz <Twulz@users.noreply.github.com>
2019-07-25 20:09:28 +1000

    simple node app with mocha tests

---
+++ index.js
@@ -0,0 +1,14 @@
+// Importing node framework
+var express = require('express');
+
+var app = express();
+
+// Respond with "hello world" for requests to root "/"
+app.get('/', function(req, res) {
+    res.send('Hello world!');
+});
+
+// Listen to port 3000 by default
+app.listen(process.env.PORT || 3000);
+
+module.exports = app;
