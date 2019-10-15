beb52148862c09e431eae51d0eaf0a3312f87a0d
Alicia <Twulz@users.noreply.github.com>
2019-10-15 20:46:41 +1100

    Add JSON and error handling hello world code

--- index.js
+++ index.js
@@ -1,14 +1,36 @@
 // Importing node framework
-var express = require('express');
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const logging = true;
 
-var app = express();
+var exports = module.exports = {};
 
-// Respond with "hello world" for requests to root "/"
-app.get('/', function(req, res) {
-    res.send('Hello world! Deployment success!');
+let app = express();
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+app.use(require('./routes/general/generalRoute.js'));
+
+// General error handler, needs to be defined AFTER all other routes
+app.use(function (err, req, res, next) {
+    // If status code hasn't changed, default to 500 server error
+    if (res.statusCode === 200) {
+        res.statusCode = 500;
+        console.error(err);
+	}
+	// Return an error code
+    res.json({
+		status: res.statusCode,
+        error: new String(err),
+    })
 });
 
-// Listen to port 3000 by default
-app.listen(process.env.PORT || 3000);
+// Start the server
+var server = app.listen(3000, function() {
+    console.log('Server started on port 3000...');
+});
 
-module.exports = app;
+exports.closeServer = function() {
+    server.close();
+};
---
+++ routes/general/generalRoute.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+const logging = true;
+
+// Respond with "hello world" for requests to root "/"}
+router.get('/', async (req, res, next) => {
+    try {
+        res.setHeader('content-type', 'application/json');
+        // if (logging) { console.log("GET request"); }
+        res.statusCode = 200;
+        res.send(JSON.stringify({"status": 200, "error": null, "response": "Hello World! Server Success!"}));
+    } catch (err) {
+        return next(err);
+    }
+});
+
+module.exports = router;
