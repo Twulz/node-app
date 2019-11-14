c9a743902cf2845a463c7c89441e02e653f0850e
Alicia <Twulz@users.noreply.github.com>
2019-11-14 21:47:14 +1100

    Jenkins test 6

--- routes/general/generalRoute.js
+++ routes/general/generalRoute.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res, next) => {
         res.setHeader('content-type', 'application/json');
         // if (logging) { console.log("GET request"); }
         res.statusCode = 200;
-        res.send(JSON.stringify({"status": 200, "error": null, "response": "Hello World! New Server5 Success!"}));
+        res.send(JSON.stringify({"status": 200, "error": null, "response": "Hello World! New Server6 Success!"}));
     } catch (err) {
         return next(err);
     }
