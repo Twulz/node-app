e7712c5977ef1ef58a24d0c3403201a35ff40e3d
Alicia <Twulz@users.noreply.github.com>
2020-01-21 20:44:19 +1100

    Allow connections from outside localhost

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(function (err, req, res, next) {
 
 // Start the server
 if(!module.parent){
-    var server = app.listen(process.env.PORT || 3000, 'localhost', function() {
+    var server = app.listen(process.env.PORT || 3000, function() {
         console.log('Server started on port 3000...');
     });
 }
