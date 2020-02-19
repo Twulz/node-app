630cf6d2e3c4d0f01e4595393559699451b987df
Alicia <Twulz@users.noreply.github.com>
2020-02-19 20:29:52 +1100

    Update environment variable read and fix vulnerabilities

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Importing node framework
+require('dotenv').config({path: './.env'});
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
