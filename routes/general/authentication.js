6ddebae5909e9f6af1bfe3ffc5952bafaeee4072
Alicia <Twulz@users.noreply.github.com>
2020-03-17 20:45:06 +1100

    Replace bcrypt with bcryptjs library

--- routes/general/authentication.js
+++ routes/general/authentication.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const Database = require('../../database/database');
 const { check, validationResult } = require('express-validator');
 
